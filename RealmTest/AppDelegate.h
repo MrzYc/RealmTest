e3000352e3e30a236faa78ca141a681a8a68e7d5
danny <danny.zhao@circcus.com>
2017-06-14 15:16:19 +0800

    jicheng

---
+++ RealmTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmTest
+//
+//  Created by Circcus on 2017/6/14.
+//  Copyright © 2017年 com.circcus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
